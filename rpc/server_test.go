aeffaa49f622432b7ddb0bbd6afedbb15176f060
tok-kkk <yunshi@republicprotocol.com>
2019-04-23 11:27:18 +1000

    initialise the branch

--- rpc/server_test.go
+++ rpc/server_test.go
@@ -1,1 +1,16 @@
 package rpc_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("rpc server", func() {
+	Context("when receiving new request", func() {
+		It("todo", func() {
+
+
+			Expect(true).Should(BeTrue())
+		})
+	})
+})
