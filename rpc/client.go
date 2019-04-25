14504e2d4516c34c2abe37e4556aac103154780e
Jaz Gulati <jaskaran.g15@gmail.com>
2019-04-25 13:48:42 +1000

    rpc: defined error message and formatting

--- rpc/client.go
+++ rpc/client.go
@@ -18,8 +18,13 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// ErrNotEnoughResultsReturned is returned if we do not get enough successful responses from Darknodes.
-var ErrNotEnoughResultsReturned = errors.New("not enough results returned")
+var (
+	// ErrNotEnoughResultsReturned is returned if we do not get enough successful responses from Darknodes.
+	ErrNotEnoughResultsReturned = errors.New("not enough results returned")
+
+	// ErrNoResultReceived is returned when the response we receive from each Darknode is nil.
+	ErrNoResultReceived = errors.New("no result received")
+)
 
 // Client is used to send RPC requests.
 type Client struct {
@@ -83,8 +88,8 @@ func (client *Client) runWorkers(n int) {
 				if response.Error != nil {
 					resp.Error = fmt.Errorf("received response error: %v", response.Error)
 				} else if err := json.Unmarshal([]byte(response.Result), &resp); err != nil {
-					client.logger.Errorf("cannot unmarshal SendMessageResponse from Darknode: %v", err)
 					close(call.Responder)
+					client.logger.Errorf("cannot unmarshal SendMessageResponse from Darknode: %v", err)
 					continue
 				}
 
@@ -98,8 +103,8 @@ func (client *Client) runWorkers(n int) {
 				if response.Error != nil {
 					resp.Error = fmt.Errorf("%v", response.Error.Message)
 				} else if err := json.Unmarshal([]byte(response.Result), &resp); err != nil {
-					client.logger.Errorf("cannot unmarshal ReceiveMessageResponse from Darknode: %v", err)
 					close(call.Responder)
+					client.logger.Errorf("cannot unmarshal ReceiveMessageResponse from Darknode: %v", err)
 					continue
 				}
 
@@ -209,7 +214,7 @@ func (client *Client) handleReceiveMessageRequest(request jsonrpc.ReceiveMessage
 		return nil
 	}
 
-	return tau.NewError(errors.New("all nodes are offline"))
+	return tau.NewError(ErrNoResultReceived)
 }
 
 type InvokeRPC struct {
