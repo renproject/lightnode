c4a3f013d305e2fb8684f87240ee336d07d01440
Jaz Gulati <jaskaran.g15@gmail.com>
2019-08-13 11:23:48 +1000

    testutils: randomise ren address

--- testutils/addr.go
+++ testutils/addr.go
@@ -33,8 +33,13 @@ func RandomMultiAddress() addr.MultiAddress {
 // NewMultiFromIPAndPort creates a new multi address with the given ip addres
 // and port, where the REN address is constant and valid.
 func NewMultiFromIPAndPort(ip string, port int) addr.MultiAddress {
-	address := addr.FromBase58("8MJcAFBHuYBeJp7zP1rXYMPTeJoYjs")
-	value := fmt.Sprintf("/ren/%s/ip4/%v/tcp/%v", address, ip, port)
+	privateKey, err := ecdsa.GenerateKey(btcec.S256(), cryptorand.Reader)
+	if err != nil {
+		panic(err)
+	}
+
+	address := addr.FromPublicKey(privateKey.PublicKey)
+	value := fmt.Sprintf("/ren/%s/ip4/%v/tcp/%v", address.ToBase58(), ip, port)
 	multi, err := addr.NewMultiAddressFromString(value)
 	if err != nil {
 		panic("could not create multi address")
