d7b6d6e231150a302d2a35f280ab601ef6684471
Jaz Gulati <jaskaran.g15@gmail.com>
2020-02-03 16:55:45 +1100

    watcher: docs and error logs

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"math/big"
 	"time"
 
@@ -18,9 +17,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// Watcher watches event logs of a specific shifter contract on Ethereum. It
-// extract information from the log and converts it an ShiftOut tx and send it
-// to validator for next stage.
+// Watcher watches for event logs for shift out transactions. These transactions
+// are then forwarded to the validator.
 type Watcher struct {
 	logger       logrus.FieldLogger
 	addr         abi.Address
@@ -57,14 +55,14 @@ func (watcher Watcher) Run(ctx context.Context) {
 	}
 }
 
-// watchLogShiftOuts checks logs which happens between current block number and
-// last checked block number. It converts any ShiftOut log to a jsonrpc.Request
-// and send it to validator for next stage.
+// watchLogShiftOuts checks logs that have occurred between current block number
+// and the last checked block number. It constructs a `jsonrpc.Request` from
+// these events and forwards them to the validator.
 func (watcher Watcher) watchLogShiftOuts(parent context.Context) {
 	ctx, cancel := context.WithTimeout(parent, watcher.PollInterval)
 	defer cancel()
 
-	// Get current block number and last checked block number
+	// Get current block number and last checked block number.
 	cur, err := watcher.currentBlockNumber(ctx)
 	if err != nil {
 		watcher.logger.Errorf("[watcher] error loading eth block header: %v", err)
@@ -72,19 +70,19 @@ func (watcher Watcher) watchLogShiftOuts(parent context.Context) {
 	}
 	last := watcher.lastCheckedBlockNumber(cur)
 
-	// Filter for all epoch events in this range of blocks
+	// Filter for all shift out events in this range of blocks.
 	shifter := watcher.pool.ShifterByAddress(watcher.addr)
 	iter, err := shifter.FilterLogShiftOut(
 		&bind.FilterOpts{
 			Context: ctx,
-			Start:   last + 1, // +1 to avoid duplication
+			Start:   last + 1, // Add one to avoid duplication.
 			End:     &cur,
 		},
 		nil,
 		nil,
 	)
 	if err != nil {
-		watcher.logger.Errorf("error filtering LogShiftOut events from=%v to=%v: %v", last, cur, err)
+		watcher.logger.Errorf("[watcher] error filtering LogShiftOut events from=%v to=%v: %v", last, cur, err)
 		return
 	}
 
@@ -92,31 +90,32 @@ func (watcher Watcher) watchLogShiftOuts(parent context.Context) {
 	for iter.Next() {
 		ref := iter.Event.ShiftID.Uint64()
 		amount := iter.Event.Amount.Uint64()
-		log.Printf("💰 New ShiftOut detected from %v, ref = %v, amount = %v SATs/ZATs", watcher.addr, ref, amount)
+		watcher.logger.Infof("[watcher] detect shift out for %v, ref=%v, amount=%v SATs/ZATs", watcher.addr, ref, amount)
 
 		// send the ShiftOut tx to validator
 		req := watcher.shiftOutToRequest(ref)
 		if ok := watcher.validator.Send(req); !ok {
-			watcher.logger.Error("[watcher] fail to send request to validator")
+			watcher.logger.Error("[watcher] failed to send request to the validator")
 			return
 		}
 	}
 	if err := iter.Error(); err != nil {
-		watcher.logger.Errorf("error iterating LogShiftOut events from=%v to=%v: %v", last, cur, err)
+		watcher.logger.Errorf("[watcher] error iterating LogShiftOut events from=%v to=%v: %v", last, cur, err)
 		return
 	}
 
 	if err := watcher.cache.Set(watcher.key(), cur, 0).Err(); err != nil {
-		watcher.logger.Errorf("error setting last checked block number in redis: %v", err)
+		watcher.logger.Errorf("[watcher] error setting last checked block number in redis: %v", err)
+		return
 	}
 }
 
-// key returns the key which we use to store the last checked block in redis.
+// key returns the key that is used to store the last checked block.
 func (watcher Watcher) key() string {
 	return fmt.Sprintf("%v_lastCheckedBlock", watcher.addr)
 }
 
-// currentBlockNumber returns the current block number of Etherem.
+// currentBlockNumber returns the current block number on Ethereum.
 func (watcher Watcher) currentBlockNumber(ctx context.Context) (uint64, error) {
 	client := watcher.pool.EthClient()
 	currentBlock, err := client.HeaderByNumber(ctx, nil)
@@ -126,18 +125,19 @@ func (watcher Watcher) currentBlockNumber(ctx context.Context) (uint64, error) {
 	return currentBlock.Number.Uint64(), nil
 }
 
-// currentBlockNumber returns the current block number of Etherem.
+// lastCheckedBlockNumber returns the last checked block number of Ethereum.
 func (watcher Watcher) lastCheckedBlockNumber(currentBlockN uint64) uint64 {
 	last, err := watcher.cache.Get(watcher.key()).Uint64()
 	if err != nil {
-		// Init the pointer with last checked block number if not set.
+		// Initialise the pointer with the last checked block number if it has
+		// not been set.
 		if err == redis.Nil {
 			if err := watcher.cache.Set(watcher.key(), currentBlockN-1, 0).Err(); err != nil {
-				watcher.logger.Panicf("cannot initialize last checked block in redis, err = %v", err)
+				watcher.logger.Panicf("[watcher] cannot initialise last checked block in redis: %v", err)
 			}
 			last = currentBlockN - 1
 		} else {
-			watcher.logger.Errorf("cannot get last checked block in redis, err = %v", err)
+			watcher.logger.Errorf("[watcher] cannot get last checked block in redis: %v", err)
 			return 0
 		}
 	}
@@ -157,7 +157,7 @@ func (watcher Watcher) shiftOutToRequest(ref uint64) http.RequestWithResponder {
 	}
 	data, err := json.Marshal(jsonrpc.ParamsSubmitTx{Tx: tx})
 	if err != nil {
-		watcher.logger.Errorf("error marshaling ShiftOut event : %v", err)
+		watcher.logger.Errorf("[watcher] error marshaling SubmitTx event: %v", err)
 		return http.RequestWithResponder{}
 	}
 	req := jsonrpc.Request{
