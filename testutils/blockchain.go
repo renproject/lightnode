a4c2b0d56801fa48f370038ca8d0407ceed9beb9
Jaz Gulati <jaskaran.g15@gmail.com>
2020-04-02 16:41:19 +1100

    confirmer_test, testutils: add basic confirmer test

--- testutils/blockchain.go
+++ testutils/blockchain.go
@@ -1,15 +1,22 @@
 package testutils
 
 import (
+	"context"
+	"encoding/binary"
 	"fmt"
+	"math"
+	"math/rand"
+	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/renproject/darknode"
+	"github.com/renproject/darknode/abi"
 	"github.com/renproject/darknode/consensus/txcheck/transform/blockchain"
 	"github.com/renproject/darknode/ethrpc"
 	"github.com/renproject/mercury/sdk/client/btcclient"
 	"github.com/renproject/mercury/sdk/client/ethclient"
 	"github.com/renproject/mercury/types"
+	"github.com/renproject/mercury/types/btctypes"
 	"github.com/sirupsen/logrus"
 )
 
@@ -29,4 +36,65 @@ func InitConnPool(logger logrus.FieldLogger, network darknode.Network, protocolA
 	}
 
 	return blockchain.New(logger, btcClient, zecClient, bchClient, ethClient, protocol)
+}
+
+type connPool struct {
+	logger                    logrus.FieldLogger
+	maxAttemptsUntilConfirmed int
+	numAttempts               map[uint64]int
+	numAttemptsMu             *sync.Mutex
+}
+
+func MockConnPool(logger logrus.FieldLogger, maxAttemptsUntilConfirmed int) *connPool {
+	return &connPool{
+		logger:                    logger,
+		maxAttemptsUntilConfirmed: maxAttemptsUntilConfirmed,
+		numAttempts:               make(map[uint64]int),
+		numAttemptsMu:             new(sync.Mutex),
+	}
+}
+
+func (cp connPool) Utxo(ctx context.Context, addr abi.Address, hash abi.B32, vout abi.U32) (btctypes.UTXO, error) {
+	cp.numAttemptsMu.Lock()
+	defer cp.numAttemptsMu.Unlock()
+
+	ref := binary.BigEndian.Uint64(hash[:])
+	cp.numAttempts[ref]++
+
+	confirmations := uint64(0)
+	// There is a 50% chance the UTXO will be marked as confirmed, until it
+	// exceeds the threshold.
+	if rand.Int()%2 == 0 || cp.numAttempts[ref] >= cp.maxAttemptsUntilConfirmed {
+		confirmations = math.MaxUint64
+
+		// Increase the number of attempts so in future queries this tx remains
+		// confirmed.
+		cp.numAttempts[ref] = cp.maxAttemptsUntilConfirmed
+	} else {
+		cp.logger.Infof("tx with hash=%v is not confirmed (attempt %d/%d)", hash.String(), cp.numAttempts[ref], cp.maxAttemptsUntilConfirmed)
+	}
+
+	return btctypes.NewUTXO(btctypes.NewOutPoint("", 0), btctypes.Amount(rand.Uint64()), []byte{}, confirmations, []byte{}), nil
+}
+
+func (cp connPool) EventConfirmations(ctx context.Context, addr abi.Address, ref uint64) (uint64, error) {
+	cp.numAttemptsMu.Lock()
+	defer cp.numAttemptsMu.Unlock()
+
+	cp.numAttempts[ref]++
+
+	confirmations := uint64(0)
+	// There is a 50% chance the UTXO will be marked as confirmed, until it
+	// exceeds the threshold.
+	if rand.Int()%2 == 0 || cp.numAttempts[ref] >= cp.maxAttemptsUntilConfirmed {
+		confirmations = math.MaxUint64
+
+		// Increase the number of attempts so in future queries this tx remains
+		// confirmed.
+		cp.numAttempts[ref] = cp.maxAttemptsUntilConfirmed
+	} else {
+		cp.logger.Infof("tx with ref=%v is not confirmed (attempt %d/%d)", ref, cp.numAttempts[ref], cp.maxAttemptsUntilConfirmed)
+	}
+
+	return confirmations, nil
 }
