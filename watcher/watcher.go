4edd16870cd420e2a9c87060e39f528a9269974a
Jaz Gulati <jaskaran.g15@gmail.com>
2020-10-07 15:43:47 +1100

    watcher: return on error

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -123,11 +123,19 @@ func (watcher Watcher) watchLogShiftOuts(parent context.Context) {
 		// Send the burn transaction to the resolver.
 		params, err := watcher.burnToParams(iter.Event.Raw.TxHash.Bytes(), pack.NewU256FromU64(pack.NewU64(amount)), pack.String(to), nonceBytes, watcher.gpubkey)
 		if err != nil {
-			watcher.logger.Infof("[watcher] cannot get params from burn transaction: %v", err)
+			watcher.logger.Errorf("[watcher] cannot get params from burn transaction: %v", err)
+			if err := watcher.cache.Set(watcher.key(), iter.Event.Raw.BlockNumber, 0).Err(); err != nil {
+				watcher.logger.Errorf("[watcher] error setting last checked block number in redis: %v", err)
+			}
+			return
 		}
 		response := watcher.resolver.SubmitTx(ctx, 0, &params, nil)
 		if response.Error != nil {
-			watcher.logger.Infof("[watcher] invalid burn transaction: %v", response.Error.Message)
+			watcher.logger.Errorf("[watcher] invalid burn transaction: %v", response.Error.Message)
+			if err := watcher.cache.Set(watcher.key(), iter.Event.Raw.BlockNumber, 0).Err(); err != nil {
+				watcher.logger.Errorf("[watcher] error setting last checked block number in redis: %v", err)
+			}
+			return
 		}
 	}
 	if err := iter.Error(); err != nil {
