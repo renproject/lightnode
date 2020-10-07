b3f7ec249aa79919d6a5feddaa2f1fad426e8497
Jaz Gulati <jaskaran.g15@gmail.com>
2020-10-07 16:36:20 +1100

    watcher: continue upon error

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -113,7 +113,7 @@ func (watcher Watcher) watchLogShiftOuts(parent context.Context) {
 		params, err := watcher.burnToParams(iter.Event.Raw.TxHash.Bytes(), pack.NewU256FromU64(pack.NewU64(amount)), pack.String(to), nonceBytes, watcher.gpubkey)
 		if err != nil {
 			watcher.logger.Errorf("[watcher] cannot get params from burn transaction (to=%v, amount=%v, nonce=%v): %v", to, amount, nonce, err)
-			return
+			continue
 		}
 		response := watcher.resolver.SubmitTx(ctx, 0, &params, nil)
 		if response.Error != nil {
