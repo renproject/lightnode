f617d422744c31ad5e0b7f0cf066c8c09e40d153
tok-kkk <yunshi@renproject.io>
2022-08-31 10:03:06 +1000

    generalizing the screener to query the address from a database table first

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -47,10 +47,10 @@ type Resolver struct {
 	bindings          binding.Bindings
 }
 
-func New(network multichain.Network, logger logrus.FieldLogger, cacher phi.Task, multiStore store.MultiAddrStore, db db.DB,
+func New(network multichain.Network, logger logrus.FieldLogger, cacher phi.Task, multiStore store.MultiAddrStore, database *sql.DB, db db.DB,
 	serverOptions jsonrpc.Options, compatStore CompatStore, versionStore v0.CompatStore, gpubkeyStore v1.GpubkeyCompatStore, bindings binding.Bindings, verifier Verifier, sanctionKey string) *Resolver {
 	requests := make(chan lhttp.RequestWithResponder, 128)
-	screener := NewScreener(sanctionKey)
+	screener := NewScreener(database, sanctionKey)
 	txChecker := newTxChecker(logger, requests, verifier, db, screener)
 	go txChecker.Run()
 
